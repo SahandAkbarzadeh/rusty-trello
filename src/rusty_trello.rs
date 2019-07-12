340fc2617e612b83a36b25244df79825ae9321a4
Sahand <sahandevs@gmail.com>
2019-07-12 11:32:31 +0430

    implement

--- src/rusty_trello.rs
+++ src/rusty_trello.rs
@@ -1,4 +1,6 @@
 use reqwest;
+use url::Url;
+
 use std::collections::HashMap;
 
 enum Authentication {
@@ -14,16 +16,27 @@ impl TrelloClient {
         TrelloClient { auth }
     }
 
-    fn is_authentication_ok(&self) -> bool {
+    fn create_url(&self, method: &str, parameters: Option<Vec<(&str, &str)>>) -> String {
+        let params: Vec<(&str, &str)> = match parameters {
+            None => vec![],
+            Some(value) => value
+        };
+
         let auth = match self.auth {
             Authentication::KeyToken(key, token) => (key, token)
         };
 
-        let url = format!("https://api.trello.com/1/{0}",
-                          format!("members/me?key={}&token={}",
-                                  auth.0, auth.1));
-        println!("Value {}", url.trim());
-        let response: HashMap<String, String> = match reqwest::get(url.trim()) {
+        let params_with_keys: Vec<(&str, &str)> = params.into_iter()
+            .chain(vec![("key", auth.0), ("token", auth.1)].into_iter())
+            .collect();
+
+        let url = format!("https://api.trello.com/1/{0}", method);
+
+        Url::parse_with_params(url.trim(), params_with_keys.into_iter()).unwrap().into_string()
+    }
+
+    fn is_authentication_ok(&self) -> bool {
+        let response: HashMap<String, String> = match reqwest::get(self.create_url("members/me", None).trim()) {
             Err(_) => return false,
             Ok(mut res) => match res.json() {
                 Err(_) => return false,
@@ -31,7 +44,6 @@ impl TrelloClient {
             }
         };
 
-        println!("{:#?}", response);
         match response.get("key") {
             None => false,
             Some(value) => value == "value"
@@ -48,5 +60,12 @@ mod test_basic_validations {
     fn test_authenticate_fail() {
         let client = TrelloClient::new(Authentication::KeyToken("wrong", "fail"));
         assert!(!client.is_authentication_ok());
+    }
+
+    #[test]
+    fn test_make_request_url() {
+        let client = TrelloClient::new(Authentication::KeyToken("wrong", "fail"));
+        assert_eq!(client.create_url("members/me", None), "https://api.trello.com/1/members/me?key=wrong&token=fail");
+        assert_eq!(client.create_url("members/me", Option::Some(vec![("test_key", "value")])), "https://api.trello.com/1/members/me?test_key=value&key=wrong&token=fail");
     }
 }
